11fc85b1c3d82e70997b20f40bd348390473e879
Petr Plsek <me@kalanys.com>
2024-04-01 18:54:01 +0200

    kw_routed_paths - bump versions, dependency analyzer

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Dependency analyzer configuration
+ * @link https://github.com/shipmonk-rnd/composer-dependency-analyser
+ */
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+$config = new Configuration();
+
+return $config
+    // ignore errors on specific packages and paths
+    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_input', __DIR__ . '/php-src/Sources/Input.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+;
